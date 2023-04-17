c56edaf058fe3c6fddb82754eb14d16800877d1c
AlanPel <83054641+AlanPel@users.noreply.github.com>
2023-04-17 13:41:15 +0200

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -16,7 +16,7 @@ export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json
 export const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json'
 export const CELO_LIST = 'https://celo-org.github.io/celo-token-list/celo.tokenlist.json'
 export const PLASMA_BNB_LIST = 'https://raw.githubusercontent.com/plasmadlt/plasma-finance-token-list/master/bnb.json'
-export const EOS_LIST = 'https://raw.githubusercontent.com/BlockABC/eos-tokens/master/tokens.json'
+export const EOS_LIST = 'https://raw.githubusercontent.com/AlanPel/eosswap/main/public/eos.json'
 
 export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST, UNI_UNSUPPORTED_LIST]
 
